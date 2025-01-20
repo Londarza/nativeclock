7ca840c1fb21fe4cc8457094082be0e890b40677
Londarza <lautaro.ondarza@gmail.com>
2025-01-20 15:18:28 -0300

    se termina app, se configura correctamente, se agregan sonidos al terminar contador, quedan detalles pero funciona todo ok

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,75 @@
 import { StyleSheet, Text, View, StatusBar, Button } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@/src/components/Header";
 import Timer from "@/src/components/Timer"
 import StartStop from "@/src/components/StartStop"
-
+import {Audio} from "expo-av"
 const colors = [
   "#A9D6E5",
    "#B8EBD0",
    "#D8B4E2",];
 
+   type TimerMode = "POMO" | "SHORT" | "BREAK";
 
 export default function Index() {
   const [hidden, setHidden] = useState(false);
+  const [isWorking,setIsWorking] = useState(false)
   const [time, setTime] = useState(25 * 60);
-  const [currentTime, setCurrentTime] = useState("POMO"| "SHORT" | "BREAK");
+  const [currentTime, setCurrentTime] = useState<TimerMode>("POMO");
   const [isActive,setIsActive] = useState(false)
 
+  const playSound = async ()=>{
+    const {sound}= await Audio.Sound.createAsync(
+        require("@/assets/Sounds/succesRing.mp3")
+    )
+    await sound.playAsync();
+}
+
+  useEffect(()=>{
+    
+    let interval: NodeJS.Timeout | undefined;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime <= 1) {
+            playSound()
+            clearInterval(interval); // Detener el intervalo cuando llegue a 0
+            handleTimerEnd(); // Manejar el fin del temporizador
+            return 0;
+          }
+          return prevTime - 1; // Reducir el tiempo en 1 segundo
+        });
+      }, 1);
+    }else{
+      // vaciar el timer
+      clearInterval(interval)
+    }
+
+    
+    return ()=>clearInterval(interval)
+  },[isActive])
+
+  const handleTimerEnd = () => {
+    setIsActive(false); // Pausar el temporizador
+    setIsWorking((prev) => !prev); // Cambiar entre trabajo y descanso
+
+    // Reiniciar el tiempo según el modo actual
+    if (currentTime === 0) {
+      setTime(5 * 60); // Pasar a Short Break
+      setCurrentTime(1);
+    } else if (currentTime === 1) {
+      setTime(25 * 60); // Pasar a Pomodoro
+      setCurrentTime(0);
+    } else {
+      setTime(25 * 60); // Volver a Pomodoro
+      setCurrentTime(0);
+    }
+  };
 
   return (
     <View
       style={[
-        styles.container, {backgroundColor:colors[currentTime]}
+        styles.container, {backgroundColor:colors[0 | currentTime]}
       ]}
     >
       <StatusBar
@@ -52,6 +101,7 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#A9D6E5",
     flex: 1,
     alignItems: "center",
     padding: 30,
